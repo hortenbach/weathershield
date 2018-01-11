84570d00cecdb66144683010c0a8ef7132b55bf1
hortenbach <s55812@beuth-hochschule.de>
2018-01-11 20:32:57 +0100

    pudate params

--- meteo.py
+++ meteo.py
@@ -1,5 +1,6 @@
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
+import json
 
 with open("./data/login.meteo", "r") as f:
         login = f.read().split()
@@ -22,19 +23,19 @@ session.fetch_token(token_url='https://auth.weather.mg/oauth/token',
 # fetch example observation data
 # the OAuth2Session will automatically handle adding authentication headers
 params = {
-    'locatedAt': '13,52',
-    'observedPeriod': 'PT0S',
-    'fields': 'airTemperatureInCelsius'
+    'fields': 'windSpeedInKilometerPerHour,clearSkyUVIndex',
+    'locatedAt': '13.40675,52.51789',
+    'validPeriod': 'PT0S',
+    'validFrom':'2018-01-11T14:00:00.000Z',
+    'validUntil':'2018-01-26T14:00:00.000Z'
 }
-#data = session.get('https://point-observation.weather.mg/search', params=params)
-
-data = session.get('https://point-forecast.weather.mg/search?fields=windSpeedInKilometerPerHour,clearSkyUVIndex&locatedAt=13.40675,52.51789&validPeriod=PT0S&validFrom=2018-01-11T14:00:00.000Z&validUntil=2018-01-26T14:00:00.000Z')
+data = session.get('https://point-forecast.weather.mg/search', params=params)
 
 jsonResponse = json.loads(data.text)
 jsonData = jsonResponse["forecasts"]
 for forecast in jsonData:
     print(forecast.get("windSpeedInKilometerPerHour"))
     print(forecast.get("clearSkyUVIndex"))
-    
+
 
 #print ("RESPONSE DATA >>> " + data.text)
